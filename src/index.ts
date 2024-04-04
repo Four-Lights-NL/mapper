2913a0756bbb48d34fe20dd33f6817240febed80
Thomas Rijpstra <thomas@fourlights.nl>
2024-04-04 16:56:42 +0200

    fix: remove `export default`

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import map from './lib/map'
 import { Flatten, Objectify } from './lib/functions'
 
 export type { MapperFn, MapperProperty, MapperConfig, MapperPropertyOptions } from './lib/map'
-export default { map, Objectify, Flatten }
+export { map, Objectify, Flatten }
