1cb93dadaaf970dcdc467033caba23c75b892a77
Thomas Rijpstra <thomas@fourlights.nl>
2024-04-08 11:55:48 +0200

    fix(plugins): add `MapperOptions` type to export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,11 @@ import map from './lib/map'
 import { Flatten, Objectify } from './lib/functions'
 
 export type { MapperPlugin } from './lib/plugins'
-export type { MapperFn, MapperProperty, MapperConfig, MapperPropertyOptions } from './lib/types'
+export type {
+	MapperFn,
+	MapperProperty,
+	MapperConfig,
+	MapperOptions,
+	MapperPropertyOptions,
+} from './lib/types'
 export { map, Objectify, Flatten }
