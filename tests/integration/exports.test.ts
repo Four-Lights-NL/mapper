e7ed3942c3178fe403c1a565080d74c8afe2714d
Thomas Rijpstra <thomas@fourlights.nl>
2024-05-13 11:32:44 +0200

    fix: allow importing utils through subpath

--- tests/integration/exports.test.ts
+++ tests/integration/exports.test.ts
@@ -1,8 +1,14 @@
 import { utils } from '@fourlights/mapper'
+import { isPlainObject } from '@fourlights/mapper/utils'
 
 describe('Mapper', () => {
 	it('should export utils', () => {
 		const user = { firstName: 'John', lastName: 'Doe', birthdate: new Date(1999, 5, 1) }
 		expect(utils.isPlainObject(user)).toBe(true)
+	})
+
+	it('should allow imports through the utils subpath`', () => {
+		const user = { firstName: 'John', lastName: 'Doe', birthdate: new Date(1999, 5, 1) }
+		expect(isPlainObject(user)).toBe(true)
 	})
 })
